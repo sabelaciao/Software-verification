f22b90aba7899441efc3c7cf9a12708f80f6137a
sabelaciao <100523102@alumnos.uc3m.es>
2026-02-06 12:16:43 +0100

    comments

--- sum.groovy
+++ sum.groovy
@@ -7,28 +7,25 @@ class SumAndSubstractionWithFloats extends Specification {
   def neg_inf = Float.NEGATIVE_INFINITY
   def pos_inf = Float.POSITIVE_INFINITY
 
-  def "Test (+INF) + (+INF)"(){
+  def "Test (+INF) + (+INF)"(){ //Test for positive infinity + positive infinity which is an extreme case
     expect:
       pos_inf == o.Sum(pos_inf, pos_inf)
   }
 
-  def "Test (+INF) + (-INF)"(){
+  def "Test (+INF) + (-INF)"(){ //Test for positive infinity + negative infinity which is an indetermination case
     expect:
       Float.NaN == o.Sum(pos_inf, neg_inf)
   }
 
-  def "Test (-INF) + (+INF)"(){
+  def "Test (-INF) + (+INF)"(){ //Test for negative infinity + positive infinity which is an indetermination case
     expect:
       Float.NaN == o.Sum(neg_inf, pos_inf)
   }
 
-  def "Test (-INF) + (-INF)"(){
+  def "Test (-INF) + (-INF)"(){ //Test for negative infinity + negative infinity which is an extreme case
     expect:
       neg_inf == o.Sum(neg_inf, neg_inf)
   }
-
-  def "Tesr (+INF) - ("
-  
 
 }
 
